55636d768ce94b11782e3cb728a74fa656c1d9df
Lateepher Hassan <queenteepher@gmail.com>
2023-02-23 19:50:07 +0000

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,12 +13,13 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 98 && n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
-				printf("%d, ", i);
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -26,7 +27,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -34,11 +37,13 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
 
