2f2243741b83650a49de848135bb5e1a01fc0a5a
Lateepher Hassan <queenteepher@gmail.com>
2023-02-23 09:06:41 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ *
+ * @a and b: parameters to be used
+ *
+ * Returns: result
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,6 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
 			}
 			else if (k >= 10)
 			{
