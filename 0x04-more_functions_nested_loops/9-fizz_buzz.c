ce670d1b4fcdfdf901cb7317f12437d41f718e48
Lateepher Hassan <queenteepher@gmail.com>
2023-02-25 03:58:28 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - function that prints from 0 to 100
+ *
+ * Return: always 0
+ */
+
+int fizz_buzz(void);
+{
+	int i;
+
+	for (i = 1 ; i < 100 ; i++)
+	{
+		if (i == 100)
+			printf("Buzz");
+		else if (i % 3 == o && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
