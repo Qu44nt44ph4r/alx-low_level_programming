60d90c1f33fe42592113831935240993779bca03
Lateepher Hassan <queenteepher@gmail.com>
2023-02-25 04:26:48 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,23 @@ int fizz_buzz(void);
 
 	for (i = 1 ; i < 100 ; i++)
 	{
-		if (i == 100)
-			printf("Buzz");
-		else if (i % 3 == o && i % 5 == 0)
-			printf("FizzBuzz ");
+		if ((i % 3 == o) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("Fizz");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+		}
 		else
-			printf("%d ", i);
+		{
+			printf("%d", i);
+		}
 	}
 	printf("\n");
+	return (0);
 }
