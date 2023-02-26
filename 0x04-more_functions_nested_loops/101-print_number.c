f27fb242608bcdd3873318af236143a00ca6271a
Lateepher Hassan <queenteepher@gmail.com>
2023-02-26 10:35:39 +0000

    Print an int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,12 +15,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n == -1;
+		n *= -1;
 		i = n;
 		_putchar('-');
 	}
 	i /= 10;
 	if (i != 0)
-	print_number
+	print_number(i);
 	_putchar((unsigned int) n % 10 + '0');
 }
