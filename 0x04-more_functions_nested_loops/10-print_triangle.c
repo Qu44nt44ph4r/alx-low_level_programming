4d3fe679a85a0087b62be9caf7665a7ba4234c0f
Lateepher Hassan <queenteepher@gmail.com>
2023-02-26 08:00:18 +0000

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,15 +19,15 @@ void print_triangle(int size)
 	{
 		for (i = 0 ; i < size ; i++)
 		{
-			for (j = size - i ; j > 1 ; j--)
-			{
-				_putchar(32);
-			}
-			for (k = 0 ; <= i ; k++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
+		for (j = size - i ; j > 1 ; j--)
+		{
+			_putchar(32);
+		}
+		for (k = 0 ; k <= i ; k++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
 		}
 	}
 }
