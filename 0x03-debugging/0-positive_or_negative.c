f00e5c8e2d033ee9be5b232dc6bce97c748e3b9f
Lateepher Hassan <queenteepher@gmail.com>
2023-02-27 07:37:45 +0000

    Check for zero

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative - function to check for positive or negative number
+ *
+ * @i: parameter to be used
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive", i);
+	else if (i == 0)
+		printf("%d is zero", i);
+	else
+		printf("%d is negative", i);
+	printf("\n");
+}
