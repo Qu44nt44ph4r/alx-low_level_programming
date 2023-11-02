681b91b6558c99f10421b8b5ba2d9cf52c03afaf
Lateepher Hassan <queenteepher@gmail.com>
2023-11-02 16:24:43 +0000

    argc is fun

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,11 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
+	int i;
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	}
 	return (0);
 }
