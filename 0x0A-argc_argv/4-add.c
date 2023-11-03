ea5ea3f837ccf5d24bdeeab0f34490d6f692c171
Lateepher Hassan <queenteepher@gmail.com>
2023-11-03 10:45:22 +0000

    argc is fun

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h> /*Required for atoi function*/
+#include <stdlib.h> /* Required for atoi function */
 #include <ctype.h>
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int add;
+    int i;
+    int j;
+    int add = 0; /* Initialize add to 0 */
 
+    /* Check if both arguments are valid positive integers */
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (!isdigit(argv[i][j]))
+            {
+                printf("Error\n"); /* Print "Error" & a new line if a non-digit symbol */
+                return 1; /* Exit with an error code */
+            }
+        }
+        add += atoi(argv[i]); /* Add the numbers */
+    }
 
-	/*Check if both arguments are valid positive integers*/
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n"); /*Print "Error" & a new line if a non-digit symbol*/
-				return (1); /*Exit with an error code*/
-			}
-		}
-	}
-
-	add =+ atoi(argv[i]); /*Add the two numbers*/
-
-	printf("%d\n", add); /*Print the result alone*/
-	return (0);
+    printf("%d\n", add); /* Print the result alone */
+    return 0;
 }
