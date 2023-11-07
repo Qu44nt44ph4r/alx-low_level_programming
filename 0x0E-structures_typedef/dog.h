710224e2215f26c66e2005f0eea9990416a58a99
Lateepher Hassan <queenteepher@gmail.com>
2023-11-07 20:26:38 +0000

    Struct is fun.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define DOG_H
 #include <stdlib.h>
 
-struct dog 
+typedef struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
