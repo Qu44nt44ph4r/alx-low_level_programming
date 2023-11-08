7863f8426d4bc7d98737f7b64ddd5a0022a12928
Lateepher Hassan <queenteepher@gmail.com>
2023-11-08 23:33:00 +0000

    Finally in printf

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
-#ifndef
-#define
+#ifndef FUNCTION_H
+#define FUNCTION_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+
 
 #endif
