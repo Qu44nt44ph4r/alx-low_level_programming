dcc61e7d10b098f84d1f34f8e38aee5f7ed2f9fe
Lateepher Hassan <queenteepher@gmail.com>
2023-11-09 00:06:56 +0000

    Finally in printf

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include "function.h"
+#include "function_pointers.h"
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
+	for (i = 0; i <= size; i++)
+		action(array[i]);
 
 }
