44e3f9b5ca304daf5aa2f9e4634779de15455d59
Lateepher Hassan <queenteepher@gmail.com>
2023-11-22 12:55:34 +0000

    A function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
+ * add_node_end - adds a new node at the beginning of a list_t list
  * @head: pointer to a pointer to the head of the list
  * @str: string to be stored in the new node
  *
@@ -10,23 +10,29 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	if (!str)
+	list_t *new_node, *temp;
+	unsigned int lenght = 0;
+
+	new_node = (list_t *) malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
+	while (str[lenght])
+		lenght++;
 
 	new_node->str = strdup(str);
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new_node->len = lenght;
+	new_node->next = NULL;
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	if (*head == NULL)
+		*head = new_node;
+	else
+
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
